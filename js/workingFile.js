f730811fc3568e041077ca1c653f0676b51613fd
SzymonCendecki <31997216+SzymekCendecki@users.noreply.github.com>
2018-10-31 20:52:32 +0100

    added jq, files for jq and new button for test changes styles behind assistance jq

---
+++ js/workingFile.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => { //start DOMContentLoaded
+  console.log("NIEWIERNE PSY RULEZ!!!!");
+
+  $(".buttonJQtoggleMain").click(function(){
+         $(".buttonJQtoggleMain").toggleClass("buttonJQtoggleNext");
+     });
+
+});//end DOMContentLoaded
